26d02bf612d951d713304852eb175db89266b977
CTSDA Dev <dev@ctsda.local>
2026-08-04 14:23:38 +0100

    fix: import BadRequestException in settings.service.ts

--- apps/api/src/settings/settings.service.ts
+++ apps/api/src/settings/settings.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, BadRequestException } from '@nestjs/common';
 import { PrismaService } from '../common/prisma.service';
 
 @Injectable()
